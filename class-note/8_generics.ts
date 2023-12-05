a36a3bd66493a39e591468b64a12d14944574016
KimJaeSung <aamoos@naver.com>
2023-12-05 16:40:42 +0900

    제너릭

---
+++ class-note/8_generics.ts
@@ -0,0 +1,41 @@
+// function logText(text){
+//     console.log(text);
+//     return text;
+// }
+// logText(10);        //숫자 10
+// logText('하이');    //문자열 하이
+// logText(true);      //진위값 true
+
+// function logText<T>(text: T):T {
+//     console.log(text);
+//     return text;
+// }
+
+// logText<string>('하이');
+
+// function logText(text: string | number){
+//     console.log(text);
+//     return text;
+// }
+
+// function logNumber(num: number){
+//     console.log(num);
+//     return num;
+// }
+
+// const a = logText('a');
+// logText(10);
+// const num = logNumber(10);
+// logText(true);
+
+function logText<T>(text: T): T{
+    console.log(text);
+    return text;
+}
+
+const str = logText<string>('abc')
+str.split('')
+const flag = logText<boolean>(true);
+
+logText('a');
+logText(10);
